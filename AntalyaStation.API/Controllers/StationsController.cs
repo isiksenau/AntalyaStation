8d23a3dbbf720739883bc12c0b886ed0ed10a0ae
Işık Sena Uysal <sena.uysal@hesas.local>
2026-07-08 17:48:40 +0300

    ilk yukleme

---
+++ AntalyaStation.API/Controllers/StationsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AntalyaStation.API.Repositories; // Repository'ye erişmek için
+
+namespace AntalyaStation.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // Adresi: api/stations olacak
+    public class StationsController : ControllerBase
+    {
+        private readonly IStationRepository _repository;
+
+        public StationsController(IStationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet] // GET isteği atıldığında çalışır
+        public async Task<IActionResult> GetStations(int page = 1, int size = 10)
+        {
+            var result = await _repository.GetPagedStationsAsync(page, size);
+            // Veriyi JSON formatında döndür
+            return Ok(new { Data = result.Data, TotalCount = result.TotalCount });
+        }
+    }
+}
---
+++ AntalyaStation.API/Repositories/IStationRepository.cs
@@ -0,0 +1,10 @@
+﻿using AntalyaStation.API.Models; // Station modelini kullanabilmek için ekledik
+
+namespace AntalyaStation.API.Repositories
+{
+    public interface IStationRepository
+    {
+        // Sayfalama (Pagination) için veriyi getiren imza
+        Task<(List<Station> Data, int TotalCount)> GetPagedStationsAsync(int pageNumber, int pageSize);
+    }
+}
